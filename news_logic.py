6fb0d2b45b218f408e65292e27ba60dc91e08bc9
kamalkhingorani <kamalkhingorani@gmail.com>
2025-06-25 01:30:24 +0530

    Update news_logic.py

--- news_logic.py
+++ news_logic.py
@@ -3,7 +3,7 @@ import requests
 def get_latest_news():
     url = "https://newsapi.org/v2/top-headlines"
     params = {
-        "apiKey": "YOUR_NEWSAPI_KEY",
+        "apiKey": "1ceb6cfbbbc54ed9b39bc27801c9b04c",
         "country": "in",
         "pageSize": 10
     }
